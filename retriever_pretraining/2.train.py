0d630374db850fdf742f9aedef004aa23f6f6742
monbious <1364143139@qq.com>
2024-01-13 21:54:45 +0800

    init

--- retriever_pretraining/2.train.py
+++ retriever_pretraining/2.train.py
@@ -35,7 +35,8 @@ from transformers import (
 from transformers.tokenization_utils_base import BatchEncoding, PaddingStrategy, PreTrainedTokenizerBase
 from transformers.trainer_utils import is_main_process
 from transformers.data.data_collator import DataCollatorForLanguageModeling
-from transformers.file_utils import cached_property, torch_required, is_torch_available, is_torch_tpu_available
+from transformers.file_utils import cached_property, is_torch_available, is_torch_tpu_available
+from transformers.utils import torch_required
 
 from simcse.models import RobertaForCL, BertForCL
 from simcse.trainers import CLTrainer
