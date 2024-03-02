d2acbb5caa1c107859d5bd62447894cb4983a1e0
monbious <1364143139@qq.com>
2024-03-02 12:26:59 +0800

    修改reference学习率

--- system_training/src/options.py
+++ system_training/src/options.py
@@ -54,8 +54,8 @@ class Options():
         self.parser.add_argument('--ranker_fixed_lr', type=bool, default=False)
 
         self.parser.add_argument('--reference_optim', type=str, default='adamw')
-        self.parser.add_argument('--reference_lr', type=float, default=0.01, help='learning rate')
-        self.parser.add_argument('--reference_weight_decay', type=float, default=0.1)
+        self.parser.add_argument('--reference_lr', type=float, default=0.001, help='learning rate')
+        self.parser.add_argument('--reference_weight_decay', type=float, default=0.01)
         self.parser.add_argument('--reference_scheduler', type=str, default='linear')
         self.parser.add_argument('--reference_scheduler_steps', type=int, default=None,
                                  help='the step meaning is same as total_step')
