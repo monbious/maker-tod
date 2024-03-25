3aa03ae99ca2389514026b803c5ef825c97154a7
monbious <1364143139@qq.com>
2024-03-25 12:32:53 +0800

    bs 1

--- system_training/run_train_condensed_mwoz_base.sh
+++ system_training/run_train_condensed_mwoz_base.sh
@@ -33,7 +33,7 @@ python train.py \
     --dk_mask True \
     --use_checkpoint \
     --per_gpu_eval_batch_size 8 \
-    --per_gpu_batch_size 2 \
+    --per_gpu_batch_size 1 \
     --eval_freq 2000 \
     --total_steps ${ES} \
     --retriever_total_steps ${ES} \
