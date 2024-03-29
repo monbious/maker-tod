547b9164351b17d82f0d26e47f002b677b7d736a
monbious <1364143139@qq.com>
2024-03-29 15:47:34 +0800

    -0.5

--- system_training/train.py
+++ system_training/train.py
@@ -386,14 +386,14 @@ def train(generator_model, retriever_model, ranker_model, generator_tokenizer, r
             train_loss = generator_loss
             if ranker_times_loss is not None:
                 train_loss = train_loss + opt.ranker_times_matrix_loss_alpha * ranker_times_loss
-                train_loss = train_loss - opt.ranker_times_matrix_loss_alpha * rest_ranker_times_loss
+                train_loss = train_loss - 0.5 * opt.ranker_times_matrix_loss_alpha * rest_ranker_times_loss
             # if rest_ranker_times_loss is not None:
             #     train_loss = train_loss + opt.ranker_times_matrix_loss_alpha * rest_ranker_times_loss
             # if ranker_scores is not None:
             #     train_loss = train_loss - ranker_model.BinaryCrossEntropy(ranker_scores, rest_ranker_scores)
             if retriever_loss is not None:
                 train_loss = train_loss + opt.generator_distill_retriever_loss_alpha * retriever_loss
-                train_loss = train_loss - opt.generator_distill_retriever_loss_alpha * rest_retriever_loss
+                train_loss = train_loss - 0.5 * opt.generator_distill_retriever_loss_alpha * rest_retriever_loss
 
             train_loss = train_loss / opt.accumulation_steps
             train_loss.backward()
