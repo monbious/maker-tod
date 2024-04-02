3d43c426bcbcb7b9d415424dc9a83d2faa59943d
monbious <1364143139@qq.com>
2024-04-02 22:16:29 +0800

    修改负样本规则

--- system_training/train.py
+++ system_training/train.py
@@ -169,7 +169,7 @@ def train(generator_model, retriever_model, ranker_model, generator_tokenizer, r
                                                         retriever_all_dbs_embeddings)  # (bs, all_db_num)
                 retriever_top_k_dbs_index = retriever_all_dbs_scores.sort(-1, True)[1][:, :opt.top_k_dbs].unsqueeze(2)  # (bs, top_k, 1)
 
-                retriever_rest_dbs_index = retriever_all_dbs_scores.sort(-1, True)[1][:, -opt.top_k_dbs:].unsqueeze(2)  # (bs, top_k, 1)
+                retriever_rest_dbs_index = retriever_all_dbs_scores.sort(-1, True)[1][:, 2:opt.top_k_dbs+2].unsqueeze(2)  # (bs, top_k, 1)
             else:
                 if opt.use_retriever_for_gt is False:
                     retriever_top_k_dbs_index = gt_db_idx[:, :opt.top_k_dbs].unsqueeze(2)  # (bs, top_k, 1)
@@ -203,7 +203,7 @@ def train(generator_model, retriever_model, ranker_model, generator_tokenizer, r
                     retriever_gt_dbs_scores = torch.gather(retriever_all_dbs_scores, 1,
                                                            gt_db_idx.long())  # (bs, gt_db_num)
                     retriever_top_k_dbs_index = retriever_gt_dbs_scores.sort(-1, True)[1][:, :opt.top_k_dbs]  # (bs, top_k)
-                    retriever_rest_dbs_index = retriever_all_dbs_scores.sort(-1, True)[1][:, -opt.top_k_dbs:].unsqueeze(2)  # (bs, top_k, 1)
+                    retriever_rest_dbs_index = retriever_all_dbs_scores.sort(-1, True)[1][:, 2:opt.top_k_dbs+2].unsqueeze(2)  # (bs, top_k, 1)
 
                     retriever_top_k_dbs_index = torch.gather(gt_db_idx, 1, retriever_top_k_dbs_index.long()).unsqueeze(2)  # (bs, top_k, 1)
 
@@ -398,11 +398,11 @@ def train(generator_model, retriever_model, ranker_model, generator_tokenizer, r
             if ranker_times_loss is not None:
                 train_loss = train_loss + opt.ranker_times_matrix_loss_alpha * ranker_times_loss
             if ranker_scores is not None:
-                train_loss = train_loss - 0.5 * ranker_model.BinaryCrossEntropy(ranker_scores, rest_ranker_scores)
+                train_loss = train_loss - 0.2 * ranker_model.BinaryCrossEntropy(ranker_scores, rest_ranker_scores)
 
             if retriever_loss is not None:
                 train_loss = train_loss + opt.generator_distill_retriever_loss_alpha * retriever_loss
-                # train_loss = train_loss - 0.5 * rest_retriever_loss
+                train_loss = train_loss - 0.2 * rest_retriever_loss
 
             train_loss = train_loss / opt.accumulation_steps
             train_loss.backward()
